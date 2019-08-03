267b672d261dc866798d13ea70917dfd6f885f04
jvcalassio <joao.victors1203@gmail.com>
2019-08-03 15:36:48 -0300

    pequenas alteracoes

--- landing-page/js/main.js
+++ landing-page/js/main.js
@@ -10,15 +10,14 @@ $(".card-header").click(function(){
     });
 });
 
-// 2. This code loads the IFrame Player API code asynchronously.
+// carrega a API do iframe
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
+// cria iframe
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -32,14 +31,12 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-// 4. The API will call this function when the video player is ready.
+// quando o player estiver pronto, inicia o video automaticamente
 function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
+// Quando o estado do player muda (state = 1), o video toca por 6s e depois para
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {

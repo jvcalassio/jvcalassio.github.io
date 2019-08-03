f7b9bfbef12fbbd987421d6ca203075ff83a29c8
jvcalassio <joao.victors1203@gmail.com>
2019-08-03 00:53:47 -0300

    first release

---
+++ landing-page/js/main.js
@@ -0,0 +1,11 @@
+$(".card-header").click(function(){
+    $(this).children().children(".fa-chevron-down").toggle();
+    $(this).children().children(".fa-chevron-right").toggle();
+    let k = this;
+    $(".card-header").each(function(i, obj){
+        if(obj != k){
+            $(this).children().children(".fa-chevron-down").css("display","none");
+            $(this).children().children(".fa-chevron-right").css("display","block");
+        }
+    });
+});

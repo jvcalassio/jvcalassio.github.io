4abd0d8576c047f9004a0087f1477059dd184e37
jvcalassio <joao.victors1203@gmail.com>
2019-08-03 20:52:29 -0300

    video lead

--- landing-page/js/main.js
+++ landing-page/js/main.js
@@ -43,26 +43,79 @@ function onPlayerReady(event) {
 
 var interval;
 var playingtime = 0;
+var sent_form_data = false;
 function onPlayerStateChange(event) {
   /*if (event.data == YT.PlayerState.PLAYING && !done) {
     setTimeout(stopVideo, 6000);
     done = true;
   }*/
-  if(player.getPlayerState() == 1){
+  if(player.getPlayerState() == 1 && !sent_form_data){
       interval = setInterval(function(){
         playingtime = player.getCurrentTime();
 
         if(playingtime >= 6){ // 6 segundos
             clearInterval(interval);
-            player.stopVideo();
             change_videotoform();
         }
         console.log(playingtime);
-      },2000);
+      },1000);
   }
 }
 
 function change_videotoform() {
+    player.pauseVideo();
     $("#player").toggle();
-    $("#form-holder").fadeIn("fast");
+    setTimeout(function() {
+        $(".video-wrapper").css("width","0");
+        $("#form-holder").fadeIn("fast");
+    }, 1000);
 }
+
+function change_formtovideo() {
+    sent_form_data = true;
+    $("#form-holder").fadeOut("fast");
+    setTimeout(function(){
+        $(".video-wrapper").css("width","640");
+        $("#player").fadeIn("fast");
+    }, 1000);
+    setTimeout(function(){
+        player.playVideo();
+        setTimeout(function(){
+            player.seekTo(8, true);
+        }, 200);
+    }, 500);
+}
+
+function validate_mail(mail){
+    var r = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return r.test(String(mail).toLowerCase());
+}
+
+function format_phone() {
+    phone = $("#input_tel").val();
+    phone = phone.replace(/[^\d]/g, "");
+    if (phone.length == 11) {
+        $("#input_tel").val(phone.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3"));
+    }
+}
+
+$("#send_data").click(function(){
+    format_phone();
+    if(validate_mail($("#input_email").val()) && $("#input_tel").val().length == 15){
+        $("#form-dados").submit();
+    }
+});
+
+$("#form-dados").submit(function(e){
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "form_submit.php",
+        data: $(this).serialize(),
+        success: function(){
+            change_formtovideo();
+        }, error: function(data) {
+            change_formtovideo();
+        }
+    });
+});

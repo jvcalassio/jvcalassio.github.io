b819dfe6a5c78709837845fba98e9768c923686e
Joao <joao.victors1203@gmail.com>
2021-02-28 17:15:25 -0300

    giltext

---
+++ giltext/assets/js/main.js
@@ -0,0 +1,22 @@
+(function($){
+    
+    let scale = '100%';
+
+    $('#1_1_scale').click(function() {
+        set_scale('100%', $(this));
+    }); 
+    $('#1_2_scale').click(function() {
+        set_scale('50%', $(this));
+    }); 
+    $('#1_6_scale').click(function() {
+        set_scale('16.6%', $(this));
+    });
+
+    function set_scale(val, btn) {
+        scale = val;
+        $('#image_show').css('background-size', scale);
+        $('.scale-selector li').removeClass('active');
+        btn.addClass('active');
+    }
+
+})(jQuery);

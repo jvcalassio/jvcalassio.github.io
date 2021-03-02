4300d6c51a997d8f500e8c5f2a8073bc7c183591
Joao <joao.victors1203@gmail.com>
2021-03-02 18:48:58 -0300

    fixes

--- giltext/assets/js/main.js
+++ giltext/assets/js/main.js
@@ -3,13 +3,13 @@
     let scale = '100%';
 
     $('#1_1_scale').click(function() {
-        set_scale('100%', $(this));
+        set_scale('200%', $(this));
     }); 
     $('#1_2_scale').click(function() {
-        set_scale('50%', $(this));
+        set_scale('100%', $(this));
     }); 
     $('#1_6_scale').click(function() {
-        set_scale('16.6%', $(this));
+        set_scale('33.3%', $(this));
     });
 
     function set_scale(val, btn) {
